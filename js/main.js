58e0b57eca1bb801a9a4fe98be6f285372aac724
Ckmena <c_menavalencia@fanshaweonline.ca>
2019-03-18 15:47:27 -0400

    animated box w/greensocks

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
-//create jar -variable-, name jar, place string in jar
-//var firstName = "Marco";
+(() => {
 
-//functions- method are preexisting fuctions
-//alert("Hello" + firstName);
 
-//modern js
-//alert(`Hello ${firstName}`)
-
-//console.log("Hello" + firstName);
 
 console.log("javascript linked up");
+
+const targetBox = document.querySelector('.box');
+
+//basic greensock animation 
+//TweenMax.to(el, time, {props});
+Function runAnimation() {
+	TweenMax.to(targetBox, 0.8, {
+		x: 450, 
+		rotation: 180,
+		scaleX: 1.5,
+		scaleY: 1.5
+	});
+}
+
+Function resetAnimation() {
+	TweenMax.to(targetBox, 0.8, {
+		x: 0, 
+		rotation: 0,
+		scaleX: 1,
+		scaleY: 1
+	});
+}
+
+targetBox.addEventListener("mouseover", runAnimation);
+targetBox.addEventListener("mouseout", resetAnimation);
+})();
